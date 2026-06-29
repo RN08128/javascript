efa19f298232f9a06326224a42cc155b216bab28
RN08128 <watitsantos@gmail.com>
2026-06-29 12:47:25 -0300

    Atualização dos itens

--- Curso 2 JS/JS/script003.js
+++
@@ -1,9 +0,0 @@
-var color = document.getElementById('color')
-
-function eventocor(){
-    color.style.backgroundColor = "Red"
-}
-
-function eventosemcor(){
-    color.style.backgroundColor = "Blue"
-}
